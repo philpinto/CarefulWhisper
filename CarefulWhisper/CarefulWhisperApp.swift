a27003da94c0b95b346d5f1f09c6979c1886bd14
Philip Pinto <philipdennispinto@icloud.com>
2026-02-20 14:21:49 -0800

    Initial Commit

---
+++ CarefulWhisper/CarefulWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarefulWhisperApp.swift
+//  CarefulWhisper
+//
+//  Created by 906 on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct CarefulWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarefulWhisperTests/CarefulWhisperTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarefulWhisperTests.swift
+//  CarefulWhisperTests
+//
+//  Created by 906 on 2/20/26.
+//
+
+import Testing
+@testable import CarefulWhisper
+
+struct CarefulWhisperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
